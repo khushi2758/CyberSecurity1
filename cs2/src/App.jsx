85027ecc33754d07d2d60128d2250b7437e99dd1
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2025-04-10 03:34:14 +0530

    routing

--- cs2/src/App.jsx
+++ cs2/src/App.jsx
@@ -1,12 +1,16 @@
-
+import{RouterProvider, createBrowserRouter} from 'react-router-dom';
+import Home from './pages/Home';
+import RootLayout from './pages/Root';
 function App() {
+  const router = createBrowserRouter([
+    {path:'/', element:  <RootLayout/>, children: [
+      {index:true, element : <Home />},
+    
 
+    ]},
+  ]);
 
-  return (
-    <>
-      <h1>hi</h1>
-    </>
-  )
+  return  <RouterProvider router={router}/>;
 }
 
 export default App
